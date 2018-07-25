c378882c0ae19442f96aaebb4837244815067838
Jeremy Cannell <jcannell007@hotmail.com>
2018-07-24 20:58:11 -0400

    added jquery module & dynamic functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import "../scss/styles";
 
 //ES6 Modules
-
 // import { square } from './app';
-
 import square from './app';
+import $ from 'jquery';
 
+let i = 3;
+
+// function setMessageText(msg) {
+//     $('#message-ES5').html(msg);
+// }
+
+let setMessageText = msg => $('#message-ES6').html(`The ES6 square of ${i}is ${square(i)}`);
 
 // ES5
-document.getElementById('message-ES5').innerHTML = "The ES5 square of 2 is " + square(2);
+setMessageText("The ES5 square of " + i + " is " + square(i));
+// document.getElementById('message-ES5').innerHTML = "The ES5 square of 2 is " + square(2);
 
 // ES6 Template Literal
-document.getElementById('message-ES6').innerHTML = `The ES6 square of 2 is ${square(2)}`;
+// setMessageText(`The ES6 square of ${i}is ${square(i)}`);
+// document.getElementById('message-ES6').innerHTML = `The ES6 square of 2 is ${square(2)}`;
