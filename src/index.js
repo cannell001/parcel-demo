a24ef8d40d67a11371208cf470095536692529be
Jeremy Cannell <jcannell007@hotmail.com>
2018-07-24 21:30:05 -0400

    click even to increase the variable

--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ import $ from 'jquery';
 
 let i = 3;
 
-// function setMessageText(msg) {
-//     $('#message-ES5').html(msg);
-// }
+function setMessageText(msg) {
+    $('#message-ES6').html(msg);
+}
 
-let setMessageText = msg => $('#message-ES6').html(`The ES6 square of ${i}is ${square(i)}`);
 
 // ES5
-setMessageText("The ES5 square of " + i + " is " + square(i));
+//setMessageText("The ES5 square of " + i + " is " + square(i));
 // document.getElementById('message-ES5').innerHTML = "The ES5 square of 2 is " + square(2);
 
 // ES6 Template Literal
-// setMessageText(`The ES6 square of ${i}is ${square(i)}`);
+setMessageText(`The ES6 square of ${i} is ${square(i)}`);
 // document.getElementById('message-ES6').innerHTML = `The ES6 square of 2 is ${square(2)}`;
+$('#message-ES6').click( () => {
+    i++;
+    setMessageText(`The ES6 square of ${i} is ${square(i)}`);
+});
