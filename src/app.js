0f20f1c8bfa8e2c1b2d56c57a1d8321b0e0bc68d
Jeremy Cannell <jcannell007@hotmail.com>
2018-07-24 20:28:53 -0400

    ES 6 modules Arrow functions Template literals

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// export default function square(x) {
+//     return x * x;
+// }
+//
+// export function square(x) {
+//     return x * x;
+// }
+
+export default x => x * x ;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "../scss/styles";
+
+//ES6 Modules
+
+// import { square } from './app';
+
+import square from './app';
+
+
+// ES5
+document.getElementById('message-ES5').innerHTML = "The ES5 square of 2 is " + square(2);
+
+// ES6 Template Literal
+document.getElementById('message-ES6').innerHTML = `The ES6 square of 2 is ${square(2)}`;
